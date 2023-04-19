0193a4061be937bb481279cbd479a4d45d3856dc
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-04-19 16:24:33 +0800

    Java Fundamental

---
+++ Conditional/src/com/dicoding/javafundamental/percabangan/IfThenElse.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean isOn = false;
+
+        if (!isOn) {
+            System.out.println("Kondisi tidak terpenuhi...");
+        } else {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+}
